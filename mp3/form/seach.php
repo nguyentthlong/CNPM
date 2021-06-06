932e0a7b80705a5ca7b1894ff98516f4422c44ec
nguyentthlong <nguyentthlong@gmail.com>
2021-06-07 01:00:33 +0700

    add folder mp3

---
+++ mp3/form/seach.php
@@ -0,0 +1,11 @@
+<div class="search_new">
+	<div class="search_new_mid">
+		<form action="./?mod=timkiem" method="post">
+		<input class="tbsearch" type="text" name="key" onclick="this.select()" onmouseover="this.focus()" onkeyup="M.sea(this.value)" maxlength="255" placeholder="Tìm bài hát ở đây ^w^">
+		<input type="submit" value="" id="btn_search" name="nuttim">
+		</form>
+	</div>
+</div>
+<div class="tab">
+    <li class="tab_selected"><a title="Nguyễn Mạnh Long" >Nhóm 17 - 60TH3| TLU</a></li>
+</div>
